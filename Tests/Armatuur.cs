4ad252b8e896c7555ed8475f7b535edd7a521622
Geerten Vink <maedaros@yahoo.com>
2021-12-21 20:34:46 +0100

    tests vertaald

---
+++ Tests/Armatuur.cs
@@ -0,0 +1,29 @@
+﻿using AdventInfi.Tests.Neenheid;
+using NUnit.Framework;
+
+namespace AdventInfi.Tests
+{
+    class Armatuur
+    {
+        [Test]
+        public void Test1()
+        {
+            var oplossing = new Oplossing(voorbeeld, 3);
+
+            Stel.ZijnGelijk(18, oplossing.BerekenDeel1());
+        }
+
+        [Test]
+        public void Test2()
+        {
+            var oplossing = new Oplossing(voorbeeld, 3);
+
+            Stel.ZijnGelijk("FZZ", oplossing.BerekenDeel2());
+        }
+
+        public const string voorbeeld = @"46 onderdelen missen
+Zoink: 9 Oink, 5 Dink
+Floep: 2 Blap, 4 Dink
+Blap: 4 Oink, 3 Dink";
+    }
+}
